ddfc4658b65d5503878e85d1bb126b0a73bffaa6
Max <nessenmaxi@googlemail.com>
2018-12-17 11:03:37 +0100

    Log Error Message

--- src/providers/error-service/error-service.ts
+++ src/providers/error-service/error-service.ts
@@ -5,8 +5,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ErrorServiceProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello ErrorServiceProvider Provider');
+  constructor(public http: HttpClient) {  }
+
+  handleError(error){
+    console.log(error);
   }
 
 }
