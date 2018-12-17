9f5f1be2e5cafce6f9a0ad6214b806228373d40d
Max <nessenmaxi@googlemail.com>
2018-12-17 14:11:00 +0100

    add Alert Handler

--- src/providers/error-service/error-service.ts
+++ src/providers/error-service/error-service.ts
@@ -1,16 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AlertServiceProvider } from '../alert-service/alert-service';
 
 
 @Injectable()
 export class ErrorServiceProvider {
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient, private alertCntrl: AlertServiceProvider) { }
 
   handleError(error) {
     switch (error.status) {
       case 401:
         console.log(error.status);
+        this
         break;
       case 409:
         console.log(error.status);
