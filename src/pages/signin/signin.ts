0d468fa03156823eb372c6d3f1b1954aab574e5f
Max <nessenmaxi@googlemail.com>
2018-12-17 12:04:12 +0100

    Add Error Handling

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpServiceProvider } from '../../providers/http-service/http-service';
+import { ErrorServiceProvider } from '../../providers/error-service/error-service';
 
 @IonicPage()
 @Component({
@@ -14,7 +15,7 @@ export class SigninPage {
   company: String;
   name: String;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private httpService: HttpServiceProvider) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private httpService: HttpServiceProvider, private errHandler: ErrorServiceProvider) { }
 
   ionViewDidLoad() {
 
@@ -31,7 +32,7 @@ export class SigninPage {
     this.httpService.setSignIn(this.email, this.password, this.company, this.name).subscribe(data =>{
       console.log(data);
     }, err => {
-      console.log(err);
+      this.errHandler.handleError(err);
     })
   }
 
