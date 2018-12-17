0006668651c5b795789999faf661f048f410cb56
Max <nessenmaxi@googlemail.com>
2018-12-17 11:11:09 +0100

    Handle error depending on StatusCode

--- src/providers/error-service/error-service.ts
+++ src/providers/error-service/error-service.ts
@@ -5,10 +5,20 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ErrorServiceProvider {
 
-  constructor(public http: HttpClient) {  }
+  constructor(public http: HttpClient) { }
 
-  handleError(error){
-    console.log(error);
+  handleError(error) {
+    switch (error.status) {
+      case 401:
+        console.log(error.status);
+        break;
+      case 409:
+        console.log(error.status);
+        break;
+      case 500:
+        console.log(error.status);
+        break;
+    }
   }
 
 }
