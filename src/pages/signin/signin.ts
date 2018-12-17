5784811dd0ca8eeffc135c08780fe42ff8954b3b
Max <nessenmaxi@googlemail.com>
2018-12-17 11:59:25 +0100

    add Alert to SignIn

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpServiceProvider } from '../../providers/http-service/http-service';
+import { AlertServiceProvider } from '../../providers/alert-service/alert-service';
 
 @IonicPage()
 @Component({
@@ -14,7 +15,7 @@ export class SigninPage {
   company: String;
   name: String;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private httpService: HttpServiceProvider) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private httpService: HttpServiceProvider, private alert: AlertServiceProvider) { }
 
   ionViewDidLoad() {
 
@@ -30,6 +31,7 @@ export class SigninPage {
     console.log("Registration called", this.email, this.password, this.company, this.name);
     this.httpService.setSignIn(this.email, this.password, this.company, this.name).subscribe(data =>{
       console.log(data);
+      this.alert.createAlertSignIn();
     }, err => {
       console.log(err);
     })
