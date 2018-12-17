45562fdd142208da7993d7e317f45ad9d6a211b0
Max <nessenmaxi@googlemail.com>
2018-12-17 11:27:26 +0100

    Add register function basic

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SigninPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-signin',
@@ -15,11 +8,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SigninPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  email: String;
+  password: String;
+  company: String;
+  name: String;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) { }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SigninPage');
+
+  }
+
+  onRegister(email, password, company, name){
+    console.log("Registration called", email, password, company, name);
   }
 
 }
