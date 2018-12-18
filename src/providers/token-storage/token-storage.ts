06533418a6e8c8e5fe21ae930655d87f8432d17b
Max <nessenmaxi@googlemail.com>
2018-12-18 14:47:12 +0100

    refactor auth

--- src/providers/token-storage/token-storage.ts
+++ src/providers/token-storage/token-storage.ts
@@ -8,11 +8,10 @@ export class TokenStorageProvider {
 
   constructor(private httpService: HttpServiceProvider) { }
 
-  getToken(email, password, cb) {
+  getToken(email, password) {
     return this.httpService.getLogin(email, password).subscribe(response => {
       console.log(response.token);
       localStorage.setItem("token", response.token);
-      return cb;
     }, err => {
       console.log(err);
     })
