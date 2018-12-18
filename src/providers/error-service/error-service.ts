1518cdebd508a9cd760e375aff5f6bc0c01d437b
Max <nessenmaxi@googlemail.com>
2018-12-18 13:18:20 +0100

    rename injection variable

--- src/providers/error-service/error-service.ts
+++ src/providers/error-service/error-service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AlertServiceProvider } from '../alert-service/alert-service';
+import { GeneralStringsProvider } from '../general-strings/general-strings';
 
 
 @Injectable()
 export class ErrorServiceProvider {
 
-  constructor(public http: HttpClient, private alertCntrl: AlertServiceProvider) { }
+  constructor(public http: HttpClient, private alertCntrl: AlertServiceProvider, private generalStrings: GeneralStringsProvider) { }
 
   handleError(error, module) {
     switch (error.status) {
