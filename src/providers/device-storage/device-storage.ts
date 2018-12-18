cb4a77396c800610a1ed32c1cd765880cda49236
Max <nessenmaxi@googlemail.com>
2018-12-18 15:21:31 +0100

    add observable class

--- src/providers/device-storage/device-storage.ts
+++ src/providers/device-storage/device-storage.ts
@@ -3,19 +3,26 @@ import { Injectable } from '@angular/core';
 import { HttpServiceProvider } from '../http-service/http-service';
 import { ErrorServiceProvider } from '../error-service/error-service';
 import { GeneralStringsProvider } from '../general-strings/general-strings';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class DeviceStorageProvider {
 
-  constructor(public http: HttpClient, private httpService:HttpServiceProvider, private errorHandler:ErrorServiceProvider, private generalStrings: GeneralStringsProvider) {  }
+  constructor(public http: HttpClient, private httpService: HttpServiceProvider, private errorHandler: ErrorServiceProvider, private generalStrings: GeneralStringsProvider) { }
 
-  getDevices(){
-    return this.httpService.getDevices().subscribe(result =>{
+  getDevices() {
+    return this.httpService.getDevices().subscribe(result => {
       console.log(result);
-    }, err =>{
+    }, err => {
       this.errorHandler.handleError(err, this.generalStrings.module_Overview)
     })
   }
+}
 
-
+export class Devices {
+  id: String;
+  model: String;
+  name: String;
+  owner: String;
+  status: String;
 }
