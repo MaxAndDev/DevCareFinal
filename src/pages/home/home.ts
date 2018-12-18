c63873e3f92f417a50a0fe5fa2e30086fc4f462b
Max <nessenmaxi@googlemail.com>
2018-12-18 14:39:28 +0100

    dealing with auth

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { TokenStorageProvider } from '../../providers/token-storage/token-storage';
 import { SigninPage } from '../signin/signin';
+import { OverviewPage } from '../overview/overview';
 
 @Component({
   selector: 'page-home',
@@ -13,12 +14,15 @@ export class HomePage {
   password: String;
 
   constructor(public navCtrl: NavController, private tokenStorage: TokenStorageProvider) {
+   
+  }
 
+  pushView(){
+    this.navCtrl.push(OverviewPage)
   }
 
   onLogin(){
-    this.tokenStorage.getToken(this.email, this.password);
-    
+    this.tokenStorage.getToken(this.email, this.password, this.pushView());    
   }
 
   onSignIn(){
--- src/providers/token-storage/token-storage.ts
+++ src/providers/token-storage/token-storage.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpServiceProvider } from '../http-service/http-service';
-import { OverviewPage } from '../../pages/overview/overview';
 
 @Injectable()
 export class TokenStorageProvider {
 
-  
 
-  constructor( private httpService: HttpServiceProvider) { }
 
-  getToken(email, password){
-    return this.httpService.getLogin(email, password).subscribe(response =>{
+  constructor(private httpService: HttpServiceProvider) { }
+
+  getToken(email, password, cb) {
+    return this.httpService.getLogin(email, password).subscribe(response => {
       console.log(response.token);
       localStorage.setItem("token", response.token);
+      return cb;
     }, err => {
       console.log(err);
     })
