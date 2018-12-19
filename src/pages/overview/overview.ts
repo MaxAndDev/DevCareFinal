c70bca492fa186c46856e68ad9ab3ed34a63d72b
Max <nessenmaxi@googlemail.com>
2018-12-19 13:29:32 +0100

    refactor getting devices

--- src/pages/overview/overview.ts
+++ src/pages/overview/overview.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DeviceStorageProvider } from '../../providers/device-storage/device-storage';
+import { HttpServiceProvider } from '../../providers/http-service/http-service';
 
 /**
  * Generated class for the OverviewPage page.
@@ -16,11 +17,19 @@ import { DeviceStorageProvider } from '../../providers/device-storage/device-sto
 })
 export class OverviewPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private deviceStorage: DeviceStorageProvider) {
-  }
+  devices = []
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private httpService: HttpServiceProvider) { }
 
   ionViewDidLoad() {
-    this.deviceStorage.getDevices();
+    this.httpService.getDevices().subscribe(result => {
+      this.extractDevices(result)
+    })
+    
+  }
+
+  extractDevices(devices){
+    this.devices = devices.devices;
   }
 
 }
--- src/providers/device-storage/device-storage.ts
+++ src/providers/device-storage/device-storage.ts
@@ -11,18 +11,16 @@ export class DeviceStorageProvider {
   constructor(public http: HttpClient, private httpService: HttpServiceProvider, private errorHandler: ErrorServiceProvider, private generalStrings: GeneralStringsProvider) { }
 
   getDevices() {
-    return this.httpService.getDevices().subscribe(result => {
-      console.log(result);
-    }, err => {
-      this.errorHandler.handleError(err, this.generalStrings.module_Overview)
-    })
+     this.httpService.getDevices().subscribe(result => {
+        console.log(result);
+     }, err => {
+       this.errorHandler.handleError(err, this.generalStrings.module_Overview);
+     })
   }
+  
+  
+
+  
 }
 
-export class Devices {
-  id: String;
-  model: String;
-  name: String;
-  owner: String;
-  status: String;
-}
+
