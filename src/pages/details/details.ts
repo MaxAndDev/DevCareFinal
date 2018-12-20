360168b61c3c9deb12a56040334f2bef93733d1c
Max <nessenmaxi@googlemail.com>
2018-12-20 09:35:42 +0100

    extract response

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpServiceProvider } from '../../providers/http-service/http-service';
+import { ErrorServiceProvider } from '../../providers/error-service/error-service';
+import { GeneralStringsProvider } from '../../providers/general-strings/general-strings';
 
 @IonicPage()
 @Component({
@@ -8,13 +11,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    let id = this.navParams.get('data');
-    console.log(id);
+  id: String;
+  model: String;
+  producer: String;
+  owner: String;
+  accessories: String[];
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private httpService: HttpServiceProvider, private errorHandler: ErrorServiceProvider, private generalStrings: GeneralStringsProvider) {
+    this.id = this.navParams.get('data');
+    console.log(this.id);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DetailsPage');
+    this.httpService.getDevice(this.id).subscribe(response =>{
+      this.extractDetails(response);
+    }, err => {
+      this.errorHandler.handleError(err, this.generalStrings.module_Details);
+    })
+  }
+
+  extractDetails(device){
+    this.model = device.model;
+    this.producer = device.producer;
+    this.owner = device.owner;
+    this.accessories = device.accessories;
+    console.log(this.model, this.producer, this.owner, this.accessories);
   }
 
 }
