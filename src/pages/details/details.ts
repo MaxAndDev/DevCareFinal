732a037d82fa48669e6637d66618fd42ee151cb1
Max <nessenmaxi@googlemail.com>
2018-12-21 11:13:18 +0100

    loop through accessories array

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -17,12 +17,12 @@ export class DetailsPage {
   producer: String;
   owner: String;
   accessories: String[];
-  headphones: String;
-  cable: String;
-  adapter: String;
-  none: String;
+  headphones: Boolean;
+  cable: Boolean;
+  adapter: Boolean;
+  none: Boolean;
 
-  admin_options:String;
+  admin_options: String;
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private httpService: HttpServiceProvider, private errorHandler: ErrorServiceProvider, private generalStrings: GeneralStringsProvider, private alertCtrl: AlertServiceProvider) {
@@ -32,32 +32,43 @@ export class DetailsPage {
   }
 
   ionViewDidLoad() {
-    this.httpService.getDevice(this.id).subscribe(response =>{
+    this.httpService.getDevice(this.id).subscribe(response => {
       this.extractDetails(response);
     }, err => {
       this.errorHandler.handleError(err, this.generalStrings.module_Details);
     })
   }
 
-  extractDetails(device){
+  extractDetails(device) {
     this.model = device.model;
     this.producer = device.producer;
     this.owner = device.owner;
     this.accessories = device.accessories;
-    if(this.accessories.length != 0){
-      this.headphones = this.accessories[0];
-      this.cable = this.accessories[1];
-      this.adapter = this.accessories[2];
+    if (this.accessories.length != 0) {
+      for (var i = 0; i < this.accessories.length; i++) {
+        if(this.accessories[i] == "Headphones"){
+          this.headphones = true;
+        }
+        if(this.accessories[i] == "Charging Cable"){
+          this.cable = true;
+        }
+        if(this.accessories[i] == "Adapter"){
+          this.adapter= true;
+        }
+        if(this.accessories[i] == "None"){
+          this.none = true;
+        }
+      }
     }
     console.log(this.model, this.producer, this.owner, this.accessories);
     console.log(this.headphones, this.cable, this.adapter);
   }
 
-  removeDev(){
+  removeDev() {
     this.httpService.deleteDevice(this.id).subscribe(response => {
       console.log(response);
       this.alertCtrl.createAlertHandler(this.generalStrings.module_deleteDev, this.generalStrings.StatusSuccess);
-    }, err =>{
+    }, err => {
       this.errorHandler.handleError(err, this.generalStrings.module_deleteDev);
     })
   }
